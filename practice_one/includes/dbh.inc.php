745ff1aa7ef8bfda685f3fff705f7a8b07204337
unknown <ganonthapayangbruno@gmail.com>
2024-03-06 15:19:02 +0100

    created a new project folder and created the config file to connect to DB with PDO

---
+++ practice_one/includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+//CONNECT TO DATABASE WITH PDO
+$dsn = ("mysql:host=localhost; dbname = msiley_db");
+$dbusername = 'root';
+$dbpassword = '';
+
+try{
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $err){
+    die('ERROR: Could not connect to the Database: ' . $err);
+}
