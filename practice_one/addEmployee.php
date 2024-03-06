5651616635db8687d7b42c1344d49766b2a203a1
unknown <ganonthapayangbruno@gmail.com>
2024-03-06 15:46:10 +0100

    handle the form submition and push data to the DB

---
+++ practice_one/addEmployee.php
@@ -0,0 +1,47 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $city = $_POST['city'];
+    $job_position = $_POST['job_positions'];
+
+    //QUERY THE DB FOR INSERTION
+
+    try{
+        require_once 'includes/dbh.inc.php'; 
+
+        $query = '
+            INSERT INTO employees 
+            (firstName, lastName, phone, email, city, job_position) 
+            VALUES (:firstName, :lastName, :phone, :email, :city, :job_position )
+        ;';
+
+    //PREPARE STATEMENT & BIND PARAMS
+            $stm = $pdo->prepare($query);
+            $stm->bindParam(":firstName", $firstName);
+            $stm->bindParam(":lastName", $lastName);
+            $stm->bindParam(":phone", $v);
+            $stm->bindParam(":email", $email);
+            $stm->bindParam(":city", $city);
+            $stm->bindParam(":job_position", $job_position);
+    //EXECUTE
+            $stm->execute();
+    //CLOSE CONNECTION & RETURN TO FORM 
+            $stm = null;
+            $pdo = null;
+            header('Location:../index.html');
+            die();
+
+
+
+    } catch(PDOException $err){
+        die('A fatal Error happened: '. $err);
+    }
+  
+
+}else{
+    header('Location:../index.html');
+}
